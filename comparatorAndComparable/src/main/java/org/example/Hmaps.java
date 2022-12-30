9324861f6f2e5e22a2fc151b706c703c013f5001
Kdusane <kunaldsn@gmail.com>
2022-12-30 12:55:01 +0530

    sdf

--- comparatorAndComparable/src/main/java/org/example/Hmaps.java
+++ comparatorAndComparable/src/main/java/org/example/Hmaps.java
@@ -5,11 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Hmaps {
-    public static void main(String[] args) {
-        Map<String, Integer> hashmap = new HashMap<>();
-        hashmap.put("kunal",1);
-        hashmap.put("siddhesh",2);
-        System.out.println(hashmap);
-        Collections.unmodifiableCollection();
 
-    }}
+    public static void main(String[] args) {
+
+    }
+
+    }
