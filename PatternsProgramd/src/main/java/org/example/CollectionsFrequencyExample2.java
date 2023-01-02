99fef5fe37d90b6f273263ffffb44197087dc391
Kdusane <kunaldsn@gmail.com>
2023-01-02 19:27:04 +0530

    dads

---
+++ PatternsProgramd/src/main/java/org/example/CollectionsFrequencyExample2.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.*;
+
+public class CollectionsFrequencyExample2 {
+    public static void main(String[] args) {
+        //Create a list object
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(1);
+        list.add(3);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println("List of elements: "+list);
+        System.out.println("\nCount all with frequency:");
+        Set<Integer> uniqueSet = new HashSet<Integer>(list);
+        for (Integer i : uniqueSet) {
+            System.out.println(i + ": " + Collections.frequency(list, i));
+        }
+    }
+}
---
+++ PatternsProgramd/src/main/java/org/example/Pattern6.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i++){
+            for (int j=4;j>=i;j--){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int x=1;x<=4;x++){
+            for (int y=1;y<=x;y++){
+                System.out.print(" ");
+            }
+            for (int z=4;z>=x;z--){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
