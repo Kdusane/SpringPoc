821941f350cdbbb3ee862307345def943bf2ea0e
Kdusane <kunaldsn@gmail.com>
2023-01-04 11:11:34 +0530

    String Swapping using 2 variables

--- PatternsProgramd/src/main/java/StringPatterns/StringSubString.java
+++ PatternsProgramd/src/main/java/StringPatterns/StringSubString.java
@@ -4,32 +4,17 @@ import java.util.*;
 
 public class StringSubString {
     public static void main(String[] args) {
-            String FIRSTNAME = "kunal";
-            String LASTNAME = "Dusane";
+        String FIRSTNAME = "sssss";
+        String LASTNAME = "ccccc";
 
-            LASTNAME = LASTNAME + FIRSTNAME;
+        LASTNAME = LASTNAME + FIRSTNAME;
 
-            FIRSTNAME = LASTNAME.substring(0,LASTNAME.length()-FIRSTNAME.length());
-            LASTNAME = LASTNAME.substring(FIRSTNAME.length());
+        FIRSTNAME = LASTNAME.substring(0, LASTNAME.length() - FIRSTNAME.length());
+        LASTNAME = LASTNAME.substring(FIRSTNAME.length());
 
-        System.out.println("LastName: "+LASTNAME);
-        System.out.println("firstname: "+FIRSTNAME);
+        System.out.println("LastName: " + LASTNAME);
+        System.out.println("firstname: " + FIRSTNAME);
 //        System.out.println(name.substring(1,2));
 
-
-        List<String> stringList = new ArrayList<>();
-        stringList.add("kunal");
-        stringList.add("kunal");
-        stringList.add("kunal");
-        stringList.add("kunal");
-        stringList.add("kunal");
-        stringList.add("kunal");
-        stringList.add("kunal");
-        stringList.add("kunal");
-
-
-
-        Set<String> strings = new HashSet<>(stringList);
-        strings.stream().forEach(System.out::println);
     }
 }
