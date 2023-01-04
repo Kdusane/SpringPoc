ee459a6d3c2f54b34927cafab607fc7fe4c3a7c0
Kdusane <kunaldsn@gmail.com>
2023-01-04 11:12:22 +0530

    number Pattern

--- PatternsProgramd/src/main/java/org/example/NumberPattern/NumberPattern2.java
+++ PatternsProgramd/src/main/java/org/example/NumberPattern/NumberPattern2.java
@@ -2,10 +2,12 @@ package org.example.NumberPattern;
 
 public class NumberPattern2 {
     public static void main(String[] args) {
-        int rows = 6;
-        for (int i=0;i<=rows;i++){
+        int rows = 4;
+        int count = 1;
+        for (int i=1;i<=rows;i++){
             for (int j=1;j<=i;j++){
-                System.out.print(i);
+                System.out.print(count++ +" ");
+
             }
             System.out.println();
         }
