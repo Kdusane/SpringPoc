267eb1635c7f0303665abfa48446c82c3e25b848
Kdusane <kunaldsn@gmail.com>
2023-01-04 11:10:39 +0530

    Arraylist Frequency

--- PatternsProgramd/src/main/java/org/example/ArrayListFrequency.java
+++ PatternsProgramd/src/main/java/org/example/ArrayListFrequency.java
@@ -1,11 +1,14 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ArrayListFrequency {
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("a","c","e","t","a","e","c");
+        Set<String> stringSet = new HashSet<>(strings);
+        for(String s1 : stringSet){
+            System.out.println(s1+" "+Collections.frequency(strings,s1));
+        }
 
     }
 }
