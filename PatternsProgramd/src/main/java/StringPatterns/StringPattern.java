80172c96d3a318842629b5e684566630b846c15b
Kdusane <kunaldsn@gmail.com>
2023-01-06 12:38:44 +0530

    StringPattern

---
+++ PatternsProgramd/src/main/java/StringPatterns/StringPattern.java
@@ -0,0 +1,16 @@
+package StringPatterns;
+
+public class StringPattern {
+    public static void main(String[] args) {
+        int  a = 97;
+        for (int i =1;i<=5;i++){
+            for (int j=4;j>=i;j--){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i;k++){
+                System.out.print((char)a+k+" ");
+            }
+            System.out.println();
+        }
+    }
+}
