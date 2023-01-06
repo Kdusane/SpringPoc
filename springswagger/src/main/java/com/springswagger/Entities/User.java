d5b472498a5a5c6919af5c75cdfb06ec868821fd
Kdusane <kunaldsn@gmail.com>
2023-01-06 12:46:05 +0530

    UserPojo

--- springswagger/src/main/java/com/springswagger/Entities/User.java
+++ springswagger/src/main/java/com/springswagger/Entities/User.java
@@ -1,4 +1,17 @@
 package com.springswagger.Entities;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class User {
+    private int id ;
+    private String name;
+    private String  phoneno;
 }
